73966488dfb1ef8ece9541a6c63650d654140454
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-09-04 01:48:33 +0100

    boiler plate setup and styled components install

---
+++ src/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  align-items: flex-start;
+  background-color: pink;
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  padding: 20px;
+  width: 100%;
+`
+export const ColumnContainer = styled.div`
+  background-color: #ebecf0;
+  width: 300px;
+  min-height: 40px;
+  margin-right: 20px;
+  border-radius: 3px;
+  padding: 8px 8px;
+  flex-grow: 0;
+`
