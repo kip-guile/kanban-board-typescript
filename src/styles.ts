a22c5411846d812eae4313ea403ac55b881a5b87
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-09-04 02:26:23 +0100

    Column component setup

--- src/styles.ts
+++ src/styles.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 export const AppContainer = styled.div`
   align-items: flex-start;
-  background-color: pink;
+  background-color: #292d3e;
+  color: #bfc7d5;
   display: flex;
   flex-direction: row;
   height: 100%;
@@ -17,4 +18,19 @@ export const ColumnContainer = styled.div`
   border-radius: 3px;
   padding: 8px 8px;
   flex-grow: 0;
+`
+
+export const ColumnTitle = styled.div`
+  padding: 6px 16px 12px;
+  font-weight: bold;
+`
+
+export const CardContainer = styled.div`
+  background-color: #fff;
+  cursor: pointer;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  max-width: 300px;
+  border-radius: 3px;
+  box-shadow: #091e4240 0px 1px 0px 0px;
 `
