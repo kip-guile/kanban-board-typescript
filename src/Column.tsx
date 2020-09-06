98baaa1a694891462b85e3f2d4d51f5cefc118f9
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-09-06 21:10:26 +0100

    add inputs

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ColumnContainer, ColumnTitle } from './styles'
+import { AddNewitem } from './AddNewItem'
 
 interface ColumnProps {
   text: string
@@ -13,6 +14,11 @@ export const Column = ({
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
       {children}
+      <AddNewitem
+        toggleButtonText='+ Add another task'
+        onAdd={console.log}
+        dark
+      />
     </ColumnContainer>
   )
 }
