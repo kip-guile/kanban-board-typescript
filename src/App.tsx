73688b4b8212d06b6819a172a86c177451c34707
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-09-06 22:28:49 +0100

    integrate context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import { Column } from './Column'
-import { Card } from './Card'
 import { AppContainer } from './styles'
 import { AddNewitem } from './AddNewItem'
+import { useAppState } from './AppStateContext'
 
 function App() {
+  const { state } = useAppState()
   return (
     <AppContainer>
-      <Column text='To Do'>
-        <Card text='Generate app scaffold' />
-      </Column>
-      <Column text='In Progress'>
-        <Card text='Learn TypeScript' />
-      </Column>
-      <Column text='Done'>
-        <Card text='Begin to use static typing' />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i} />
+      ))}
       <AddNewitem toggleButtonText='+ Add another list' onAdd={console.log} />
     </AppContainer>
   )
