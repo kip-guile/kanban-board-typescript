72f4f0db1a6139571817ab055300de2c647b34e3
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-11 16:40:36 +0100

    cards done

--- src/Column.tsx
+++ src/Column.tsx
@@ -16,21 +16,38 @@ interface ColumnProps {
 }
 
 export const Column = ({ text, index, id, isPreview }: ColumnProps) => {
-  const [, drop] = useDrop({
-    accept: 'COLUMN',
-    hover(item: DragItem) {
-      const dragIndex = item.index
-      const hoverIndex = index
-
-      if (dragIndex === hoverIndex) {
-        return
-      }
-
-      dispatch({type: 'MOVE_LIST', payload: {dragIndex, hoverIndex}})
-      item.index = hoverIndex
-    }
-  })
   const { state, dispatch } = useAppState()
+  const [, drop] = useDrop({
+    accept: ["COLUMN", "CARD"],
+    hover(item: DragItem) {
+      if (item.type === 'COLUMN') {
+        const dragIndex = item.index
+        const hoverIndex = index
+
+        if (dragIndex === hoverIndex) {
+          return
+        }
+
+        dispatch({type: 'MOVE_LIST', payload: {dragIndex, hoverIndex}})
+        item.index = hoverIndex
+      } else {
+        const dragIndex = item.index
+        const hoverIndex = 0
+        const sourceColumn = item.columnId
+        const targetColumn = id
+        if (sourceColumn === targetColumn) {
+          return
+        }
+        dispatch({
+          type: "MOVE_TASK",
+          payload: { dragIndex, hoverIndex, sourceColumn, targetColumn }
+        })
+        item.index = hoverIndex
+        item.columnId = targetColumn
+      }
+      }
+    })
+  
   const ref = useRef<HTMLDivElement>(null)
   const { drag } = useItemDrag({ type: 'COLUMN', id, index, text })
   drag(drop(ref))
@@ -38,7 +55,7 @@ export const Column = ({ text, index, id, isPreview }: ColumnProps) => {
     <ColumnContainer isPreview={isPreview} ref={ref} isHidden={isHidden(isPreview, state.draggedItem, "COLUMN", id)}>
       <ColumnTitle>{text}</ColumnTitle>
       {state.lists[index].tasks.map((task, i) => (
-        <Card text={task.text} key={task.id} index={i} />
+        <Card text={task.text} key={task.id} index={i} id={task.id} columnId={id} />
       ))}
       <AddNewitem
         toggleButtonText='+ Add another task'
